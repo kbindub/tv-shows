1b1eb01a281cc72ba2a077dec8fd69efc84cefbb
Kongara <bindu.kongara@capgemini.com>
2020-11-23 12:25:13 +0530

    test file changes

--- tests/unit/services/tvShowsApi.spec.js
+++ tests/unit/services/tvShowsApi.spec.js
@@ -1,24 +1,22 @@
 import axios from 'axios';
 import { getListOfShows,getShowsBasedOnsearch, getShowDetailsBasedOnId} from '@/services/tvShowsApi.js';
+import { showsList, showDetails } from '../testData'
 
 jest.mock('axios');
 
 describe('getListOfShows', () => {
-    it('Should fetch showslist successfully from an API', async () => {
-      const data = [{"name" : "test"}, {"name" : "testing"}];   
-      axios.get.mockImplementationOnce(() => Promise.resolve(data));  
-      await expect(getListOfShows()).resolves.toEqual(data);
+    it('Should fetch showslist successfully from an API', async () => {   
+      axios.get.mockImplementationOnce(() => Promise.resolve(showsList));  
+      await expect(getListOfShows()).resolves.toEqual(showsList);
     });
 
     it('Should fetch showslist based on searchkey', async () => {
-        const data = [{"name" : "test"}, {"name" : "testing"}];   
-        axios.get.mockImplementationOnce(() => Promise.resolve(data));  
-        await expect(getShowsBasedOnsearch()).resolves.toEqual(data);
+        axios.get.mockImplementationOnce(() => Promise.resolve(showsList));  
+        await expect(getShowsBasedOnsearch()).resolves.toEqual(showsList);
     });
 
     it('Should fetch a show details based on id', async () => {
-        const data = {"name" : "test"};   
-        axios.get.mockImplementationOnce(() => Promise.resolve(data));  
-        await expect(getShowDetailsBasedOnId()).resolves.toEqual(data);
+        axios.get.mockImplementationOnce(() => Promise.resolve(showDetails));  
+        await expect(getShowDetailsBasedOnId()).resolves.toEqual(showDetails);
     });
 });
--- tests/unit/views/detailsOfShow.spec.js
+++ tests/unit/views/detailsOfShow.spec.js
@@ -18,7 +18,7 @@ describe('Inside DetailsOfShow.vue Component', () => {
     });
 
     it('Should have a DetailsOfShow component', () => {
-        expect(detailsOfShowWrapper.html()).toContain('<detailsofshow-stub></detailsofshow-stub>');
+        expect(detailsOfShowWrapper.html()).toContain('<details-of-show-stub></details-of-show-stub>');
     });
 
 })
